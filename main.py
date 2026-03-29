e20330ee3d885b26f16eb26675cd7cc876269835
Thai Koehnle <takoehnle@gmail.com>
2026-03-29 01:19:34 -0500

    Train the classifier (pac) and check its accuracy

--- main.py
+++ main.py
@@ -34,3 +34,12 @@ tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
 tfidf_train = tfidf_vectorizer.fit_transform(x_train)
 # just convert text from test set to numeric features
 tfidf_test = tfidf_vectorizer.transform(x_test)
+
+# initialize a PassiveAggressiveClassifier
+pac = PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
+
+#predict on test set and calculate accuracy
+y_pred = pac.predict(tfidf_test)
+score = accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
